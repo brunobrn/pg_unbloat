453f553650170803794066de245d823dac8768f7
brunobrn <bruno.farias@stone.com.br>
2023-09-24 18:04:51 -0300

    FIX: removing drop extension line

--- pg_unbloat/clean_migration.sql
+++ pg_unbloat/clean_migration.sql
@@ -3,4 +3,3 @@ DROP FUNCTION IF EXISTS public.get_table_size(text);
 DROP FUNCTION IF EXISTS public.oid2text(oid);
 DROP FUNCTION IF EXISTS public.get_columns_for_create_as(oid);
 DROP FUNCTION IF EXISTS public.get_storage_param(oid);
-DROP EXTENSION IF EXISTS PGSTATTUPLE;
